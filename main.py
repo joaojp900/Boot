d54bc8628c6aff845f3eb9c9169c350967302306
edgar34c <edgararaujocorrea@gmail.com>
2022-04-30 22:06:19 -0300

    Selenium funcionando.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-a=input('digite algo')
+a=input('digite algo: ')
 print('{}'.format(a))
 
 navegador = webdriver.Chrome()
+navegador.get("https://www.google.com.br/")
+navegador.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(a)
+navegador.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]').click()
