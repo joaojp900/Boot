b51a623f2f498efd453985254a1af48b1866b969
edgar34c <edgararaujocorrea@gmail.com>
2022-04-30 22:12:01 -0300

    Selenium funcionando, corrigido pagina de pesquisa.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -5,6 +7,6 @@ a=input('digite algo: ')
 print('{}'.format(a))
 
 navegador = webdriver.Chrome()
-navegador.get("https://www.google.com.br/")
+navegador.get("https://images.google.com.br/imghp?source=mog&gl=br&gws_rd=ssl")
 navegador.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(a)
-navegador.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]').click()
+navegador.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/button/div/span').click()
